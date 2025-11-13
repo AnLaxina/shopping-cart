4117ed5873acea3bef2c6dc7340758a7ac9dc60d
AnLaxina <anilovlaxina@gmail.com>
2025-11-12 21:41:40 -0800

    feat: add appropriate font

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import {createRoot} from 'react-dom/client'
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import routes from "./route.jsx";
 
-import './index.css'
+import "./styles/reset.css";
+import "./styles/typography.css";
 
 const router = createBrowserRouter(routes);
 createRoot(document.getElementById('root')).render(

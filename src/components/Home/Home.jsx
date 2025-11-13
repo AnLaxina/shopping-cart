f10fbf22e249c9d9e19216d8d5416d5cce6223a8
AnLaxina <anilovlaxina@gmail.com>
2025-11-13 09:06:20 -0800

    feat(home): add basic content for the homepage

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,9 @@
+import styles from "./home.module.css";
+
+export default function Home() {
+    return (
+        <div className={styles.home}>
+            <h2>Homepage</h2>
+            <p>Welcome to the amazing store of... stuff! Here you'll find a random assortment of things!</p>
+        </div>)
+}
--- src/route.jsx
+++ src/route.jsx
@@ -1,10 +1,14 @@
 // All this does is make routes for `main.jsx`
 import App from "./App.jsx";
+import Home from "./components/Home/Home.jsx";
 
 const routes = [
     {
         path: "/",
-        element: <App/>
+        element: <App/>,
+        children: [
+            {index: true, element: <Home/>}
+        ]
     }
 ]
 

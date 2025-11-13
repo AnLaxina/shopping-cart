bc060e208d7133ece7c964dae4af2a5babfcf5bd
AnLaxina <anilovlaxina@gmail.com>
2025-11-13 09:40:05 -0800

    feat(header): add appropriate links for the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import './App.css';
-import {Outlet} from "react-router-dom";
+import {Link, Outlet} from "react-router-dom";
 
 function App() {
 
     return (
         <>
-            <header><h1>Shopping Cart App</h1></header>
+            <header>
+                <h1>Shopping Cart App</h1>
+                <nav>
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/shop">Shop</Link></li>
+                        <li><Link to="/cart">Cart</Link></li>
+                    </ul>
+                </nav>
+            </header>
             <main><Outlet/></main>
             <footer>2025 AnLaxina</footer>
         </>

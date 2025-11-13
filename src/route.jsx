d4eeb55c960053a26503a685d26cff5b1a443f8b
AnLaxina <anilovlaxina@gmail.com>
2025-11-13 09:24:20 -0800

    feat: add working routes for the Cart and Shop page

--- src/route.jsx
+++ src/route.jsx
@@ -1,13 +1,17 @@
 // All this does is make routes for `main.jsx`
 import App from "./App.jsx";
 import Home from "./components/Home/Home.jsx";
+import Shop from "./components/Shop/Shop.jsx";
+import Cart from "./components/Cart/Cart.jsx";
 
 const routes = [
     {
         path: "/",
         element: <App/>,
         children: [
-            {index: true, element: <Home/>}
+            {index: true, element: <Home/>},
+            {path: "/shop", element: <Shop/>},
+            {path: "/cart", element: <Cart/>}
         ]
     }
 ]

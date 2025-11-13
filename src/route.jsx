9d0f81206572148f694df3f8f6bd48a8c485bee8
AnLaxina <anilovlaxina@gmail.com>
2025-11-12 20:56:07 -0800

    feat: add basic setup for routing

---
+++ src/route.jsx
@@ -0,0 +1,11 @@
+// All this does is make routes for `main.jsx`
+import App from "./App.jsx";
+
+const routes = [
+    {
+        path: "/",
+        element: <App/>
+    }
+]
+
+export default routes;

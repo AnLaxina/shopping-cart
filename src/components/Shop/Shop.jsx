cc5d931c9459436a6a20b4ff6ad4e9d37799c562
AnLaxina <anilovlaxina@gmail.com>
2025-11-13 16:39:56 -0800

    feat(shop): add sample `shopitem`s

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,5 +1,14 @@
+import styles from "./shop.module.css";
+import ShopItem from "../ShopItem/ShopItem";
+
 export default function Shop() {
-    return (
-        <p>This is the shop page!</p>
-    )
+  return (
+    <div>
+      <h2>ShopItem</h2>
+      <div className={styles.shopItems}>
+        <ShopItem itemName="Chicken" />
+        <ShopItem itemName="かのじょ" />
+      </div>
+    </div>
+  );
 }

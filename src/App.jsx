b337db30d3714e235aadd9063ba3f9f5a3700160
AnLaxina <anilovlaxina@gmail.com>
2025-11-15 15:12:31 -0800

    feat(app): add state for the cart count

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,36 @@
-import './App.css';
-import {Link, Outlet} from "react-router-dom";
+import "./App.css";
+import { Link, Outlet } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
+  const [cartCount, setCartCount] = useState(0);
 
-    return (
-        <>
-            <header>
-                <h1>Shopping Cart App</h1>
-                <nav>
-                    <ul>
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/shop">Shop</Link></li>
-                        <li><Link to="/cart">Cart</Link></li>
-                    </ul>
-                </nav>
-            </header>
-            <main><Outlet/></main>
-            <footer>2025 AnLaxina</footer>
-        </>
-    )
+  return (
+    <>
+      <header>
+        <h1>Shopping Cart App</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/shop">Shop</Link>
+            </li>
+            <li>
+              <Link to="/cart">
+                Cart {cartCount !== 0 && " (" + cartCount + ")"}
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <Outlet context={[cartCount, setCartCount]} />
+      </main>
+      <footer>2025 AnLaxina</footer>
+    </>
+  );
 }
 
 export default App;

8ef8ed7c2b2d5678117091226b42c4415ad5dcd8
AnLaxina <anilovlaxina@gmail.com>
2025-11-15 15:34:40 -0800

    feat(shopitem): update cart automatically when item is added
    Addtionally, made the input field required when no number is entered

--- src/components/ShopItem/ShopItem.jsx
+++ src/components/ShopItem/ShopItem.jsx
@@ -1,17 +1,26 @@
+import { useOutletContext } from "react-router-dom";
 import styles from "./shopitem.module.css";
 
 export default function ShopItem({ itemName }) {
+  const [cartCount, setCartCount] = useOutletContext();
+  function addCartCount(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const quantity = Number(formData.get("quantity"));
+
+    setCartCount(cartCount + quantity);
+  }
   return (
     <div className={styles.shopItem}>
       <img src="/vite.svg" alt="Vite icon" />
       <h3>{itemName}</h3>
       {/* Will need to make the action equal something else than # */}
-      <form action="#">
+      <form action="#" onSubmit={(event) => addCartCount(event)}>
         <div className={styles.quantityContainer}>
           <label htmlFor="quantity">Quantity </label>
-          <input type="number" id="quantity" min="0" />
+          <input required type="number" name="quantity" id="quantity" min="0" />
         </div>
-        <button type="button">Add to Cart</button>
+        <button type="submit">Add to Cart</button>
       </form>
     </div>
   );

18b36adb106e8bf3ab910be9857cf002338b6a15
AnLaxina <anilovlaxina@gmail.com>
2025-11-17 20:14:16 -0800

    feat: make `shopitem` more responsive for mobile

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -3,11 +3,16 @@ import ShopItem from "../ShopItem/ShopItem";
 
 export default function Shop() {
   return (
-    <div>
+    <div className={styles.shop}>
       <h2>ShopItem</h2>
       <div className={styles.shopItems}>
         <ShopItem itemName="Chicken" />
         <ShopItem itemName="かのじょ" />
+        <ShopItem itemName="Burger" />
+        <ShopItem itemName="Toy" />
+        <ShopItem itemName="Toy" />
+        <ShopItem itemName="Toy" />
+        <ShopItem itemName="Toy" />
       </div>
     </div>
   );
--- src/components/ShopItem/ShopItem.jsx
+++ src/components/ShopItem/ShopItem.jsx
@@ -2,32 +2,31 @@ import { useOutletContext } from "react-router-dom";
 import styles from "./shopitem.module.css";
 
 export default function ShopItem({ itemName }) {
-    const [cartCount, setCartCount] = useOutletContext();
-    function addCartCount(event) {
-        event.preventDefault();
-        const formData = new FormData(event.target);
-        const quantity = Number(formData.get("quantity"));
+  const [cartCount, setCartCount] = useOutletContext();
+  function addCartCount(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const quantity = Number(formData.get("quantity"));
 
-        setCartCount(cartCount + quantity);
-    }
-    return (
-        <div className={styles.shopItem}>
-            <img src="/vite.svg" alt="Vite icon" />
-            <h3>{itemName}</h3>
-            {/* Will need to make the action equal something else than # */}
-            <form action="#" onSubmit={(event) => addCartCount(event)}>
-                <div className={styles.quantityContainer}>
-                    <label htmlFor="quantity">Quantity </label>
-                    <input
-                        required
-                        type="number"
-                        name="quantity"
-                        id="quantity"
-                        min="0"
-                    />
-                </div>
-                <button type="submit">Add to Cart</button>
-            </form>
+    setCartCount(cartCount + quantity);
+  }
+  return (
+    <div className={styles.shopItem}>
+      <img src="/vite.svg" alt="Vite icon" />
+      <h3>{itemName}</h3>
+      {/* Will need to make the action equal something else than # */}
+      <form action="#" onSubmit={(event) => addCartCount(event)}>
+        <div className={styles.quantityContainer}>
+          <label htmlFor="quantity" className={styles.regularStyle}>
+            Quantity{" "}
+          </label>
+          <label htmlFor="quantity" className={styles.mobileStyle}>
+            Qty.{" "}
+          </label>
+          <input required type="number" name="quantity" id="quantity" min="0" />
         </div>
-    );
+        <button type="submit">Add to Cart</button>
+      </form>
+    </div>
+  );
 }

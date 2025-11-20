23eb4cdf78c9fa8ed8423eb397bcedeea1f7114f
AnLaxina <anilovlaxina@gmail.com>
2025-11-20 15:43:57 -0800

    feat(cart): add styling & basic layout for cart page

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,5 +1,30 @@
+import styles from "./cart.module.css";
+
 export default function Cart() {
-    return (
-        <p>This is the cart page!</p>
-    )
+  return (
+    <div className={styles.cart}>
+      <h2>Your Cart</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Product</th>
+            <th>Image</th>
+            <th>Description</th>
+            <th>Quantity</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Product Name</td>
+            <td>
+              <img src="/vite.svg" alt="Vite logo" />
+            </td>
+            <td>Cool Description</td>
+            <td>3</td>
+          </tr>
+        </tbody>
+      </table>
+      <p>This is the cart page!</p>
+    </div>
+  );
 }

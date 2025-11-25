0f67cf4d22abae0cfa6f54afe02731700adb073d
AnLaxina <anilovlaxina@gmail.com>
2025-11-25 12:09:17 -0800

    feat(home): add better styling and text

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,17 @@
 import styles from "./home.module.css";
 
 export default function Home() {
-    return (
-        <div className={styles.home}>
-            <h2>Homepage</h2>
-            <p>Welcome to the amazing store of... stuff! Here you'll find a random assortment of things!</p>
-        </div>)
+  return (
+    <div className={styles.home}>
+      <h2>Welcome to StuffMart</h2>
+      <br />
+      <p>
+        Your one-stop shop for... stuff! Yup.. this is all for the homepage!
+      </p>
+      <p>
+        Also, there's no checkout page... so... add products to your heart's
+        content!
+      </p>
+    </div>
+  );
 }
